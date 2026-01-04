cd235a1ac4e18c843dc3ae6b5ccf0c0620342b3f
Bilal <bilal@gmail.com>
2026-01-04 20:43:35 +0200

    Update frontend API to use Render backend

--- dr-online/src/utils/api.js
+++ dr-online/src/utils/api.js
@@ -1,4 +1,4 @@
-const API = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API = process.env.REACT_APP_API_URL || "https://doctor-online-backend.onrender.com/api";
 
 export async function authFetch(path, options = {}) {
   const token = localStorage.getItem("token");
